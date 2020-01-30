2acee33e594abb3c5c277c9281436461a9e9d2f5
S-Toky <stokyr29@gmail.com>
2020-01-30 18:14:18 +0300

    add mailer google

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :welcome_send
   has_one :cart
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
