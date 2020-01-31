fe84330802fbc3ab622ad8fc0a52b57bf2d0418b
S-Toky <stokyr29@gmail.com>
2020-01-31 10:37:24 +0300

    add mailer run

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_one :cart
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
