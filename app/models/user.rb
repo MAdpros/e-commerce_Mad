bc17897aabb1712802e605f4f984d202112c87b9
S-Toky <stokyr29@gmail.com>
2020-02-03 17:25:31 +0300

    add mailjet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :welcome_send
   has_one :cart
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  def welcome_send
+UserMailer.welcome_email(self).deliver_now
+end
 
 end
